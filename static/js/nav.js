e7b25713da4388f1bb0b54b4ea01c657b3b9c226
Jarno Ruokamo <jarno.ruokamo@gmail.com>
2020-07-12 12:36:19 +0300

    BulmaJS npm:n kautta
    Kokeillaanpas

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,4 +1,11 @@
 window.addEventListener('DOMContentLoaded', function() {
+
+    var link = document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = document.getElementById('favicon').getAttribute('src');
+    document.getElementsByTagName('head')[0].appendChild(link);
+
     // Get all "navbar-burger" elements
     const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
