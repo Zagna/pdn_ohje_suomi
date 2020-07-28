e4cccfd3e3df7e7d5d06c301c344c0890bf88ffb
Jarno Ruokamo <jarno.ruokamo@gmail.com>
2020-07-28 19:49:28 +0300

    Touch paremmin?

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,15 @@
 window.addEventListener('DOMContentLoaded', function() {
 
-    var link = document.createElement('link');
-    link.type = 'image/x-icon';
-    link.rel = 'shortcut icon';
-    link.href = document.getElementById('favicon').getAttribute('src');
-    document.getElementsByTagName('head')[0].appendChild(link);
+	var link = document.createElement('link');
+	link.type = 'image/x-icon';
+	link.rel = 'shortcut icon';
+	link.href = document.getElementById('favicon').getAttribute('src');
+	document.getElementsByTagName('head')[0].appendChild(link);
 
+	document.addEventListener('touchend', function(event) {
+		document.querySelectorAll('.navbar-item.has-dropdown').forEach(el => el.classList.remove('is-active'));
+		if (event.target.classList.contains('navbar-link')) event.target.parentNode.classList.add('is-active');
+	}, false);
 });
 
 import Tabs from '@vizuaalog/bulmajs/src/plugins/tabs';
