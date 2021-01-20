12b237c43194314c2a6387512a4fbb4a4ffa873d
Jarno Ruokamo <jarno.ruokamo@gmail.com>
2021-01-20 21:03:18 +0200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,11 @@ module.exports = {
 	plugins: {
 		'@fullhuman/postcss-purgecss': {
 			content: ['./layouts/**/*.html'],
+			safelist: [
+					'dl', 'dt', 'dd', 'ol', 'ul', 'li',
+					'table', 'thead', 'th', 'tbody', 'td', 'tr',
+					'h1', 'h2', 'h3', 'h4', 'h5', 'p', 'span'
+				],
 		},
 	},
 };
