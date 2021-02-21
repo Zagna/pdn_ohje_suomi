9b0d4436a8ed7f3c151eded8fc9cbf19368da766
Jarno Ruokamo <jarno.ruokamo@gmail.com>
2021-02-21 18:21:36 +0200

    Defer kokeilu

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,26 @@
-window.addEventListener('DOMContentLoaded', function () {
-	var link = document.createElement('link');
-	link.type = 'image/x-icon';
-	link.rel = 'shortcut icon';
-	var favicon = document.getElementById('favicon');
-	if (favicon.currentSrc) {
-		link.href = favicon.currentSrc;
-	}
-	else {
-		link.href = favicon.getAttribute('src');
-	}
-	document.getElementsByTagName('head')[0].appendChild(link);
+import supportsWebP from 'supports-webp';
+import '@vizuaalog/bulmajs/src/plugins/tabs';
+import '@vizuaalog/bulmajs/src/plugins/navbar';
 
-	document.addEventListener('touchend', function (event) {
-		document.querySelectorAll('.navbar-item.has-dropdown').forEach(el => el.classList.remove('is-active'));
-		if (event.target.classList.contains('navbar-link')) event.target.parentNode.classList.add('is-active');
-	}, false);
-});
+var link = document.createElement('link');
+link.type = 'image/x-icon';
+link.rel = 'shortcut icon';
+var favicon = document.getElementById('favicon');
+if (favicon.currentSrc) {
+	link.href = favicon.currentSrc;
+}
+else {
+	link.href = favicon.getAttribute('src');
+}
+document.getElementsByTagName('head')[0].appendChild(link);
+
+document.addEventListener('touchend', function (event) {
+	document.querySelectorAll('.navbar-item.has-dropdown').forEach(el => el.classList.remove('is-active'));
+	if (event.target.classList.contains('navbar-link')) event.target.parentNode.classList.add('is-active');
+}, false);
 
 supportsWebP.then(supported => {
 	if (!supported) {
 		document.body.classList.add("no-webp");
 	}
 });
-
-import supportsWebP from 'supports-webp';
-import '@vizuaalog/bulmajs/src/plugins/tabs';
-import '@vizuaalog/bulmajs/src/plugins/navbar';
