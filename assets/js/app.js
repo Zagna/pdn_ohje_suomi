1d64e9faa0b207c9f4c9906edb6f266d5bd1613b
Jarno Ruokamo <jarno.ruokamo@gmail.com>
2021-06-25 20:53:45 +0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,19 @@
 import supportsWebP from 'supports-webp';
 
-var elements = document.getElementsByClassName("tab-radio");
-
-for (var i = 0; i < elements.length; i++) {
-    elements[i].addEventListener('change', function (event) {
-		document.querySelectorAll('.tab-item').forEach(el => el.classList.remove('is-active'));
-		event.target.labels[0].parentElement.classList.add('is-active');
-	}, false);
-}
-
-document.addEventListener('touchend', function (event) {
-	document.querySelectorAll('.navbar-item.has-dropdown').forEach(el => el.classList.remove('is-active'));
-	if (event.target.classList.contains('navbar-link')) event.target.parentNode.classList.add('is-active');
-}, false);
-
 supportsWebP.then(supported => {
 	if (!supported) {
 		document.body.classList.add("no-webp");
 	}
 });
+
+document.querySelectorAll(".tab-radio").forEach(radio =>
+	radio.addEventListener('change', function (event) {
+		document.querySelectorAll('.tab-item').forEach(item => item.classList.remove('is-active'));
+		event.target.labels[0].parentElement.classList.add('is-active');
+	}, false)
+);
+
+document.addEventListener('touchend', function (event) {
+	document.querySelectorAll('.navbar-item.has-dropdown').forEach(navitem => navitem.classList.remove('is-active'));
+	if (event.target.classList.contains('navbar-link')) event.target.parentNode.classList.add('is-active');
+}, false);
