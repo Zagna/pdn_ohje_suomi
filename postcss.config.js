ed8e203112c1cae2836073c4ba553fe90c036ffd
Jarno Ruokamo <jarno.ruokamo@gmail.com>
2021-09-25 08:16:09 +0300

    Päivitys

--- postcss.config.js
+++ postcss.config.js
@@ -3,10 +3,10 @@ module.exports = {
 		'@fullhuman/postcss-purgecss': {
 			content: ['./layouts/**/*.html'],
 			safelist: [
-					'dl', 'dt', 'dd', 'ol', 'ul', 'li',
-					'table', 'thead', 'th', 'tbody', 'td', 'tr',
-					'h1', 'h2', 'h3', 'h4', 'h5', 'p', 'span'
-				],
+				'dl', 'dt', 'dd', 'ol', 'ul', 'li',
+				'table', 'thead', 'th', 'tbody', 'td', 'tr',
+				'h1', 'h2', 'h3', 'h4', 'h5', 'p', 'span'
+			],
 		},
 		'postcss-csso': {
 			restructure: true,
