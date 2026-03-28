67fef267ebcc6f046700ebb3bf416238c55e2cd9
Jarno Ruokamo <jarno.ruokamo@gmail.com>
2026-03-28 21:11:10 +0200

    @supports

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,3 @@
-/*! npm.im/supports-webp 3.0.0 */
-var supportsWebP = new Promise(resolve => {
-	const image = new Image();
-	image.addEventListener('error', () => resolve(false));
-	image.addEventListener('load', () => resolve(image.width === 1));
-	image.src = 'data:image/webp;base64,UklGRiQAAABXRUJQVlA4IBgAAAAwAQCdASoBAAEAAwA0JaQAA3AA/vuUAAA=';
-}).catch(() => false);
-
-supportsWebP.then(supported => {
-	if (!supported) {
-		document.body.classList.add("no-webp");
-	}
-});
-
 document.querySelectorAll(".tabs-wrapper input[type='radio']").forEach(radio =>
 	radio.addEventListener('change', function(event) {
 		document.querySelectorAll('.tab').forEach(item => item.classList.remove('active'));
